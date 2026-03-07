15d7099f6fb44bea6e515045164995d3e111969e
Yusuke <67366917+Yusuke4869@users.noreply.github.com>
2026-03-07 23:38:11 +0900

    chore: update eslint config (#598)
    * chore: update eslint config
    
    * chore: update lint and format scripts
    
    * chore: remove unused eslint dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,49 +1,29 @@
-import { fixupPluginRules } from "@eslint/compat";
 import eslint from "@eslint/js";
-import pluginTs from "@typescript-eslint/eslint-plugin";
-import tseslintParser from "@typescript-eslint/parser";
-import prettier from "eslint-config-prettier";
-import pluginImport from "eslint-plugin-import";
-import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginImport from "eslint-plugin-import";
+import prettier from "eslint-config-prettier";
 
-const config = tseslint.config(
+/** @type {import("eslint").Linter.Config[]} */
+export default [
   {
-    ignores: ["node_modules", "dist"],
-  },
-  {
-    files: ["**/*.js", "**/*.ts"],
-    languageOptions: {
-      globals: { ...globals.node },
-      ecmaVersion: "latest",
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      parser: tseslintParser,
-    },
-    settings: {
-      "import/resolver": {
-        typescript: {},
-      },
-      "import/parsers": {
-        "@typescript-eslint/parser": [".js", ".ts"],
-      },
-    },
-    plugins: {
-      import: fixupPluginRules(pluginImport),
-      "@typescript-eslint": pluginTs,
-    },
+    ignores: ["node_modules", "dist", "eslint.config.mjs"],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.strict,
   ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylistic,
   ...tseslint.configs.stylisticTypeChecked,
-  prettier,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: pluginImport.flatConfigs.recommended.plugins,
     rules: {
+      ...pluginImport.flatConfigs.recommended.rules,
+      ...pluginImport.flatConfigs.typescript.rules,
       "import/consistent-type-specifier-style": "error",
       "import/newline-after-import": "error",
       "import/no-duplicates": "error",
@@ -59,7 +39,12 @@ const config = tseslint.config(
         },
       ],
     },
-  }
-);
-
-export default config;
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+  },
+  prettier,
+];
